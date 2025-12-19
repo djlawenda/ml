95fc3d4c78aecc2540d8a88512dd2ba8d8d9d1da
djlawenda <rusak.lukasz@gmail.com>
2025-12-19 16:40:47 +0800

    ML sync: 2025-12-19 16:40:47

--- src/finish-synch.sh
+++ src/finish-synch.sh
@@ -4,5 +4,5 @@ cd "/home/lukasz/Documents/code/py/ml"
 # Stage new/modified files but ignore caches if you used .gitignore
 git add .
 # Auto message with timestamp; edit as you like
-git commit -m "Logseq sync: $(date +'%Y-%m-%d %H:%M:%S')"
+git commit -m "ML sync: $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin main
